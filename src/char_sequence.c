f85e00d4921339384eb7c8ef1177fa4766c0323b
KLZ-0 <adrian@kalazi.com>
2020-10-28 05:03:31 +0100

    Implement char sequence

---
+++ src/char_sequence.c
@@ -0,0 +1,74 @@
+#include <string.h>
+
+#include "char_sequence.h"
+
+////// Private
+
+// define the struct here to hide the interface
+struct CharSequence {
+	char *data;
+	size_t len;
+	size_t mem_size;
+};
+
+/**
+ * Resizes the allocated memory for the char sequence, if its length is equal to the allocated memory size
+ * This allocates enough memory to add one character to the char sequence
+ * @param charseq the char sequence to be resized
+ * @return true on success, false on allocation error
+ */
+bool charseq_mayresize(charseq_t charseq) {
+	if ((charseq->len + 1) == charseq->mem_size) {
+		size_t oldsize = charseq->mem_size;
+		charseq->mem_size *= GROWTH_FACTOR;
+		charseq->data = realloc(charseq->data, sizeof(char) * (charseq->mem_size));
+		if (charseq->data == NULL) {
+			return false;
+		}
+		// clear the new memory address
+		memset(charseq->data + oldsize, 0, oldsize);
+	}
+	return true;
+}
+
+////// Public
+
+charseq_t charseq_init() {
+	charseq_t charseq = calloc(1, sizeof(struct CharSequence));
+	if (charseq == NULL) {
+		return NULL;
+	}
+
+	charseq->data = calloc(INITIAL_ALLOC_SIZE, sizeof(char));
+	if (charseq->data == NULL) {
+		return NULL;
+	}
+
+	charseq->mem_size = INITIAL_ALLOC_SIZE;
+	return charseq;
+}
+
+bool charseq_add(charseq_t charseq, char c) {
+	if (!charseq_mayresize(charseq)) {
+		return false;
+	}
+
+	charseq->data[charseq->len++] = c;
+
+	return true;
+}
+
+size_t charseq_len(charseq_t charseq) {
+	return charseq->len;
+}
+
+char const *charseq_get(charseq_t charseq) {
+	return charseq->data;
+}
+
+void charseq_free(charseq_t charseq) {
+	if(charseq->data != NULL) {
+		free(charseq->data);
+	}
+	free(charseq);
+}
---
+++ src/char_sequence.h
@@ -0,0 +1,46 @@
+#ifndef IFJ_CHAR_SEQUENCE_H
+#define IFJ_CHAR_SEQUENCE_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define INITIAL_ALLOC_SIZE 16
+#define GROWTH_FACTOR 2
+
+typedef struct CharSequence *charseq_t;
+
+/**
+ * Allocates and initializes a char sequence
+ * @return the initialized char sequence
+ */
+charseq_t charseq_init();
+
+/**
+ * Appends a character to the end of a char sequence, the char sequence may be resized if neccessary
+ * @param charseq
+ * @param c character to be appended
+ * @return true on success, false on allocation error
+ */
+bool charseq_add(charseq_t charseq, char c);
+
+/**
+ * Getter for the char sequence length
+ * @param charseq
+ * @return actual length of the string in bytes (should be equal to strlen)
+ */
+size_t charseq_len(charseq_t charseq);
+
+/**
+ * Getter for the char sequence data
+ * @param charseq
+ * @return c string representing the char sequence
+ */
+char const *charseq_get(charseq_t charseq);
+
+/**
+ * Free the allocated char sequence
+ * @param charseq
+ */
+void charseq_free(charseq_t charseq);
+
+#endif //IFJ_CHAR_SEQUENCE_H
