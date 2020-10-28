c8049b86817eabc326539a31f4e5dc48afd36652
Kevin Lackó <kevinlacko.kl@gmail.com>
2020-10-28 15:16:55 +0100

    Changed flist type

--- src/container/forward_list.c
+++ src/container/forward_list.c
@@ -1,43 +1,37 @@
 #include <stdlib.h>
-#include <stdint.h>
+#include <string.h>
 #include <assert.h>
 
 #include "forward_list.h"
 
 struct forward_list
 {
-	void* data;
-	flist_handle_t next;
+	void *data;
+	flist_t next;
 };
 
-void flist_push_front( flist_handle_t* flist, void* data, size_t data_size )
-{
-	assert( flist && data && data_size );
-	flist_handle_t new_node = (flist_handle_t)malloc( sizeof( flist_t ) );
-	assert( new_node );
+void flist_push_front(flist_t *flist, void *data, size_t data_size) {
+	assert(flist && data && data_size);
+	flist_t new_node = (flist_t)malloc(sizeof(struct forward_list));
+	assert(new_node);
 
-	new_node->data = malloc( data_size );
-	assert( new_node->data );
+	new_node->data = malloc(data_size);
+	assert(new_node->data);
 
-	for( size_t i = 0; i < data_size; i++ )
-	{
-		*( (uint8_t*)( new_node->data) + i  ) = *( (uint8_t*)( data ) + i );
-	}
+	memcpy(new_node->data, data, data_size);
 
 	new_node->next = *flist;
 	*flist = new_node;
 }
 
-void flist_clear( flist_handle_t* flist )
-{
-	flist_handle_t current = *flist;
-	flist_handle_t next;
+void flist_clear(flist_t *flist) {
+	flist_t current = *flist;
+	flist_t next;
 
-	while( current != NULL )
-	{
+	while (current != NULL) {
 		next = current->next;
-		free( current->data );
-		free( current );
+		free(current->data);
+		free(current);
 		current = next;
 	}
 
--- src/container/forward_list.h
+++ src/container/forward_list.h
@@ -3,14 +3,12 @@
 
 #include <stdbool.h>
 
-typedef struct forward_list flist_t;
+typedef struct forward_list *flist_t;
 
-typedef flist_t* flist_handle_t;
+#define flist_create(name) flist_t name = NULL;
 
-#define flist_create(name) flist_handle_t name = NULL;
+void flist_push_front(flist_t *flist, void *data, size_t data_size);
 
-void flist_push_front( flist_handle_t* flist, void* data, size_t data_size );
-
-void flist_clear( flist_handle_t* flist );
+void flist_clear(flist_t *flist);
 
 #endif // !FORWARD_LIST_H
