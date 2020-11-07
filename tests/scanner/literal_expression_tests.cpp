2ab5d535ee276c938a2c564e1c453f5f503d075e
KLZ-0 <adrian@kalazi.com>
2020-11-07 01:11:47 +0100

    Add basic literal expressions

---
+++ tests/scanner/literal_expression_tests.cpp
@@ -0,0 +1,66 @@
+#include <gtest/gtest.h>
+
+#include "scanner_tests.h"
+
+TEST_F(ScannerTest, expression_basic) {
+	fprintf(stream, "5 + 5");
+
+	// 5
+	scanner_retrieve_token(scanner, token);
+	ASSERT_EQ(token->type, TK_INT_LIT);
+	EXPECT_EQ(token->param.i, 5);
+
+	// +
+	scanner_retrieve_token(scanner, token);
+	EXPECT_EQ(token->type, TK_PLUS);
+
+	// 5
+	scanner_retrieve_token(scanner, token);
+	ASSERT_EQ(token->type, TK_INT_LIT);
+	EXPECT_EQ(token->param.i, 5);
+
+	scanner_retrieve_token(scanner, token);
+	EXPECT_EQ(token->type, TK_EOF);
+}
+
+TEST_F(ScannerTest, expression_basic_spaces) {
+	fprintf(stream, "\t5 \t     \t+   \t 5\t ");
+
+	// 5
+	scanner_retrieve_token(scanner, token);
+	ASSERT_EQ(token->type, TK_INT_LIT);
+	EXPECT_EQ(token->param.i, 5);
+
+	// +
+	scanner_retrieve_token(scanner, token);
+	EXPECT_EQ(token->type, TK_PLUS);
+
+	// 5
+	scanner_retrieve_token(scanner, token);
+	ASSERT_EQ(token->type, TK_INT_LIT);
+	EXPECT_EQ(token->param.i, 5);
+
+	scanner_retrieve_token(scanner, token);
+	EXPECT_EQ(token->type, TK_EOF);
+}
+
+TEST_F(ScannerTest, expression_basic_nospaces) {
+	fprintf(stream, "5+5");
+
+	// 5
+	scanner_retrieve_token(scanner, token);
+	ASSERT_EQ(token->type, TK_INT_LIT);
+	EXPECT_EQ(token->param.i, 5);
+
+	// +
+	scanner_retrieve_token(scanner, token);
+	EXPECT_EQ(token->type, TK_PLUS);
+
+	// 5
+	scanner_retrieve_token(scanner, token);
+	ASSERT_EQ(token->type, TK_INT_LIT);
+	EXPECT_EQ(token->param.i, 5);
+
+	scanner_retrieve_token(scanner, token);
+	EXPECT_EQ(token->type, TK_EOF);
+}
