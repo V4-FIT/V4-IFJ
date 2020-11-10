c003facf74e0f53329038fbc042e95fd504bd6eb
KLZ-0 <adrian@kalazi.com>
2020-11-10 23:58:17 +0100

    Expand rules that need to be done

--- src/rules.c
+++ src/rules.c
@@ -179,27 +179,94 @@ static int rule_type(scanner_t scanner) { // 4x must return EPS
 }
 
 // TODO
-static int rule_statements(scanner_t scanner);
-// Statements -> ε
-static int rule_statement_n(scanner_t scanner); // 2x
-static int rule_statement(scanner_t scanner); // 6x
-static int rule_var_define(scanner_t scanner);
-// Var_define -> ε
-static int rule_assignment(scanner_t scanner);
-// Assignment -> ε
-static int rule_ids(scanner_t scanner);
-static int rule_id_n(scanner_t scanner); // 2x
-static int rule_expressions(scanner_t scanner);
-static int rule_expression_n(scanner_t scanner); // 2x
-static int rule_assignOp(scanner_t scanner); // 5x
-static int rule_conditionals(scanner_t scanner);
-static int rule_conditional_n(scanner_t scanner); // 3x
-static int rule_conditional(scanner_t scanner); // 2x
-static int rule_iterative(scanner_t scanner);
-static int rule_functionCall(scanner_t scanner); // 2x
-static int rule_funParams(scanner_t scanner);
-static int rule_funParam_n(scanner_t scanner); // 2x
-static int rule_funParam(scanner_t scanner); // 2x
-static int rule_term(scanner_t scanner); // 2x
-static int rule_literal(scanner_t scanner); // 2x
-static int rule_return(scanner_t scanner);
+
+static int rule_statements(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
+// LL: Statements -> ε
+
+static int rule_statement_n(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_statement(scanner_t scanner) { // 6x
+	return EXIT_SUCCESS;
+}
+
+static int rule_var_define(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
+// LL: Var_define -> ε
+
+static int rule_assignment(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
+// LL: Assignment -> ε
+
+static int rule_ids(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
+static int rule_id_n(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_expressions(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
+static int rule_expression_n(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_assignOp(scanner_t scanner) { // 5x
+	return EXIT_SUCCESS;
+}
+
+static int rule_conditionals(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
+static int rule_conditional_n(scanner_t scanner) { // 3x
+	return EXIT_SUCCESS;
+}
+
+static int rule_conditional(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_iterative(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
+static int rule_functionCall(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_funParams(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
+static int rule_funParam_n(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_funParam(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_term(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_literal(scanner_t scanner) { // 2x
+	return EXIT_SUCCESS;
+}
+
+static int rule_return(scanner_t scanner) {
+	return EXIT_SUCCESS;
+}
+
