ca00565cebe6734a11c6da6dc27e5d153bc23739
Kevin Lackó <kevinlacko.kl@gmail.com>
2020-11-10 17:38:17 +0100

    Symbol table crude implementation

--- src/symtable.c
+++ src/symtable.c
@@ -1,6 +1,8 @@
-#include <assert.h>
-
 #include "symtable.h"
+
+#include <assert.h>
+#include <string.h>
+
 #include "forward_list.h"
 #include "hash_map.h"
 
@@ -11,6 +13,10 @@
 struct symtable {
 	flist_t tables;
 };
+
+hmap_t symtable_front(symtable_t symtable) {
+	return *(hmap_t *)flist_front(symtable->tables);
+}
 
 ////// Public
 
@@ -45,7 +51,7 @@ bool symtable_enter_scope(symtable_t symtable) {
 
 void symtable_exit_scope(symtable_t symtable) {
 	assert(symtable && !flist_empty(symtable->tables));
-	hmap_t hmap = *(hmap_t *)flist_front(symtable->tables);
+	hmap_t hmap = symtable_front(symtable);
 
 	for (hmap_iterator_t it = hmap_begin(hmap); !hmap_it_eq(it, hmap_end(hmap)); hmap_it_next(it)) {
 		symbol_t sym = *(symbol_t *)hmap_get_value(it);
@@ -59,7 +65,123 @@ void symtable_exit_scope(symtable_t symtable) {
 	flist_pop_front(symtable->tables);
 }
 
+bool symtable_has_symbol(symtable_t symtable, token_t id_token) {
+	symbol_ref_t symbol_ref = symtable_find(symtable, id_token);
+	return symbol_valid(symbol_ref);
+}
 
+bool symtable_has_func(symtable_t symtable, token_t id_token) {
+	symbol_ref_t symbol_ref = symtable_find(symtable, id_token);
+	if (symbol_valid(symbol_ref)) {
+		if (symbol_ref.symbol->type == ST_FUNC) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool symtable_has_var(symtable_t symtable, token_t id_token) {
+	symbol_ref_t symbol_ref = symtable_find(symtable, id_token);
+	if (symbol_valid(symbol_ref)) {
+		if (symbol_ref.symbol->type == ST_VAR) {
+			return true;
+		}
+	}
+	return false;
+}
+
+symbol_ref_t symtable_find(symtable_t symtable, token_t id_token) {
+	assert(symtable && id_token);
+	assert(!flist_empty(symtable->tables));
+	assert(id_token->type == TK_IDENTIFIER);
+	assert(id_token->param.s);
+
+	hmap_key_t key = id_token->param.s;
+	symbol_ref_t symbol_ref;
+	symbol_ref.symbol = NULL;
+	for (flist_iterator_t it = flist_begin(symtable->tables); flist_it_valid(it); flist_it_next(it)) {
+		hmap_t hmap = *(hmap_t *)flist_get(it);
+		symbol_ref.it = hmap_find(hmap, key);
+		if (hmap_it_valid(symbol_ref.it)) {
+			symbol_ref.symbol = hmap_get_value(symbol_ref.it);
+			break;
+		}
+	}
+
+	return symbol_ref;
+}
+
+symbol_ref_t symtable_insert(symtable_t symtable, token_t id_token, symbol_type_t symbol_type) {
+	assert(symtable && id_token);
+	assert(!flist_empty(symtable->tables));
+	assert(id_token->type == TK_IDENTIFIER);
+	assert(id_token->param.s);
+
+	const char *name = id_token->param.s;
+	hmap_t hmap = symtable_front(symtable);
+
+	symbol_ref_t symbol_ref;
+	symbol_ref.symbol = NULL;
+	symbol_ref.it = hmap_find_add(hmap, name);
+
+	if (hmap_it_valid(symbol_ref.it)) {
+
+		symbol_t symbol;
+		symbol.type = symbol_type;
+		symbol.name = hmap_get_key(symbol_ref.it);
+		// init type specific data
+		if (symbol_type == ST_FUNC) {
+			symbol.func.param_count = 0;
+			symbol.func.return_count = 0;
+			symbol.func.param_list = flist_init(sizeof(data_type_t));
+			if (symbol.func.param_list == NULL) {
+				symbol_ref.it = hmap_end(hmap);
+				return symbol_ref;
+			}
+			symbol.func.return_list = flist_init(sizeof(data_type_t));
+			if (symbol.func.return_list == NULL) {
+				flist_free(symbol.func.param_list);
+				symbol_ref.it = hmap_end(hmap);
+				return symbol_ref;
+			}
+		} else {
+			symbol.var.data_type = DT_UNDEFINED;
+		}
+		// set symbol data
+		hmap_set_value(symbol_ref.it, &symbol);
+		symbol_ref.symbol = hmap_get_value(symbol_ref.it);
+	}
+
+	return symbol_ref;
+}
+
+bool symbol_func_add_param(symbol_ref_t symbol_ref, data_type_t data_type) {
+	assert(symbol_valid(symbol_ref));
+	assert(symbol_ref.symbol->type == ST_FUNC);
+
+	flist_t list = symbol_ref.symbol->func.param_list;
+	symbol_ref.symbol->func.param_count++;
+	return flist_push_front(list, &data_type);
+}
+
+bool symbol_func_add_return(symbol_ref_t symbol_ref, data_type_t data_type) {
+	assert(symbol_valid(symbol_ref));
+	assert(symbol_ref.symbol->type == ST_FUNC);
+
+	flist_t list = symbol_ref.symbol->func.return_list;
+	symbol_ref.symbol->func.return_count++;
+	return flist_push_front(list, &data_type);
+}
+
+void symbol_var_set_data(symbol_ref_t symbol_ref, sym_var_t sym_var_data) {
+	assert(symbol_valid(symbol_ref));
+	assert(symbol_ref.symbol->type == ST_VAR);
+	symbol_ref.symbol->var = sym_var_data;
+}
+
+bool symbol_valid(symbol_ref_t symbol_ref) {
+	return hmap_it_valid(symbol_ref.it);
+}
 
 void symtable_free(symtable_t symtable) {
 	assert(symtable);
--- src/symtable.h
+++ src/symtable.h
@@ -1,9 +1,12 @@
 #ifndef SYMTABLE_H
 #define SYMTABLE_H
 
+#include "forward_list.h"
+
 #include <stdbool.h>
 
-#include "forward_list.h"
+#include "hash_map.h"
+#include "tokens.h"
 
 typedef struct symtable *symtable_t;
 
@@ -16,13 +19,15 @@ typedef enum {
 	DT_INTEGER,
 	DT_FLOAT64,
 	DT_STRING,
-	DT_BOOL
+	DT_BOOL,
+	DT_UNDEFINED
 } data_type_t;
 
 typedef struct {
 	data_type_t data_type;
 	//TODO: SYMTABLE - add required attributes for variables
 } sym_var_t;
+
 
 typedef struct {
 	unsigned int param_count;
@@ -31,7 +36,6 @@ typedef struct {
 	flist_t return_list;
 	//TODO: SYMTABLE - revise / add required attributes for functions
 } sym_func_t;
-
 
 typedef struct {
 	const char *name;
@@ -42,10 +46,35 @@ typedef struct {
 	};
 } symbol_t;
 
+typedef struct {
+	hmap_iterator_t it;
+	symbol_t *symbol;
+}symbol_ref_t;
+
 symtable_t symtable_init();
 
+bool symtable_enter_scope(symtable_t symtable);
 
+void symtable_exit_scope(symtable_t symtable);
 
-void symtable_free();
+bool symtable_has_symbol(symtable_t symtable, token_t id_token);
+
+bool symtable_has_func(symtable_t symtable, token_t id_token);
+
+bool symtable_has_var(symtable_t symtable, token_t id_token);
+
+symbol_ref_t symtable_find(symtable_t symtable, token_t id_token);
+
+symbol_ref_t symtable_insert(symtable_t symtable, token_t id_token, symbol_type_t symbol_type);
+
+bool symbol_func_add_param(symbol_ref_t symbol_ref, data_type_t data_type);
+
+bool symbol_func_add_return(symbol_ref_t symbol_ref, data_type_t data_type);
+
+void symbol_var_set_data(symbol_ref_t symbol_ref, sym_var_t sym_var_data);
+
+bool symbol_valid(symbol_ref_t symbol_ref);
+
+void symtable_free(symtable_t symtable);
 
 #endif // !SYMTABLE_H
