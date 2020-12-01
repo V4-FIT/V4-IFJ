2409b62e0ac6af8e4338e213fadc762c4aae1e9a
KLZ-0 <adrian@kalazi.com>
2020-12-01 19:51:45 +0100

    Add a few instructions for expression operations + Fix missing frame

--- src/generator.c
+++ src/generator.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <ctype.h>
 
+#include "error.h"
+
 /**
  * Some notes on code generation
  *
@@ -16,6 +18,8 @@
  * The root of argument stack is NIL -> for variadic arguments as in print(...)
  * RETURN VAUES are in order from LEFT to RIGHT [fun(2, 1, 0) (0, 1, 2)] (order of stack pushes)
  * TL;DR: Function arguments and return values are stored on the stack until nil is met (stack root value must be nil)
+ *
+ * Expression operations are implemented similar to FPU (stack) operations
  *
  * Allowed label chars:
  *  _ - $ & % * ! ?
@@ -197,61 +201,11 @@ static void builtin_define() {
 	builtin_float2int();
 }
 
-////// Code segment generation -> public interface implementation
-
-void gen_init() {
-	header();
-	builtin_define();
-}
-
-void gen_finish() {
-	INSTRUCTION("LABEL !_main");
-
-#ifndef NDEBUG
-	INSTRUCTION("BREAK");
-#endif
-
-	// just for the sake of completeness
-	fflush(stdout);
-}
-
-/// Function definition
-
-void gen_func_begin(const char *identifier) {
-	COMMENT("Begin function");
-
-	INSTRUCTION("LABEL ", identifier);
-	INSTRUCTION("PUSHFRAME");
-}
-
-void gen_func_init_frame() {
-	INSTRUCTION("CREATEFRAME");
-}
-
-void gen_func_init_stack() {
-	INSTRUCTION("PUSHS nil@nil");
-}
-
-void gen_func_param(const char *identifier) {
-	INSTRUCTION("DEFVAR TF@", identifier);
-	INSTRUCTION("POPS TF@", identifier);
-}
-
-void gen_func_end() {
-	INSTRUCTION("POPFRAME");
-	INSTRUCTION("RETURN");
-
-	COMMENT("End funtion");
-}
-
-/// Function call
-
-void gen_func_call(const char *identifier) {
-	INSTRUCTION("CALL ", identifier);
-}
-
-void gen_func_call_arg_literal(token_t token) {
+static void push_token(token_t token) {
 	switch (token->type) {
+		case TK_IDENTIFIER:
+			INSTRUCTION("PUSHS TF@", token->param.s);
+			break;
 		case TK_INT_LIT:
 			INSTRUCTION_PART("PUSHS int@");
 			printf("%lld", token->param.i);
@@ -278,38 +232,125 @@ void gen_func_call_arg_literal(token_t token) {
 	}
 }
 
+////// Code segment generation -> public interface implementation
+
+void gen_init() {
+	header();
+	builtin_define();
+}
+
+void gen_finish() {
+	INSTRUCTION("LABEL !_main");
+
+#ifndef NDEBUG
+	INSTRUCTION("BREAK");
+#endif
+
+	// just for the sake of completeness
+	fflush(stdout);
+}
+
+/// Function definition
+
+void gen_func_begin(const char *identifier) {
+	COMMENT("Begin function");
+
+	INSTRUCTION("LABEL ", identifier);
+	INSTRUCTION("PUSHFRAME");
+	INSTRUCTION("CREATEFRAME");
+}
+
+void gen_func_init_stack() {
+	INSTRUCTION("PUSHS nil@nil");
+}
+
+void gen_func_param(const char *identifier) {
+	INSTRUCTION("DEFVAR TF@", identifier);
+	INSTRUCTION("POPS TF@", identifier);
+}
+
+void gen_func_end() {
+	INSTRUCTION("POPFRAME");
+	INSTRUCTION("RETURN");
+
+	COMMENT("End funtion");
+}
+
+/// Function call
+
+void gen_func_call(const char *identifier) {
+	INSTRUCTION("CALL ", identifier);
+}
+
+/**
+ * Pushes the function call argument onto the argument stack
+ * @param token literal or identifier token
+ */
+void gen_func_call_arg(token_t token) {
+	push_token(token);
+}
+
 void gen_var_define(const char *identifier) {
 	INSTRUCTION("DEFVAR TF@", identifier);
 }
 
-void gen_var_assign_id(const char *to, const char *from) {
-	INSTRUCTION("MOVE TF@", to, " TF@", from);
+/**
+ * Pushes the literal/variable onto the stack for further computation
+ * @param token literal or identifier token
+ */
+void gen_var_literal(token_t token) {
+	push_token(token);
 }
 
-void gen_var_assign_literal(const char *to, tk_param_t param, token_type_t token_type) {
-	switch (token_type) {
-		case TK_INT_LIT:
-			INSTRUCTION_PART("MOVE TF@", to, " int@");
-			printf("%lld", param.i);
-			INSTRUCTION_END();
+/**
+ * Assigns expression result to a frame variable
+ * @param identifier
+ */
+void gen_var_assign_expr_result(const char *identifier) {
+	INSTRUCTION("POPS TF@", identifier);
+}
+
+/**
+ * Generates an operator specific instruction
+ * @param operator token type, one of {+ - * /}
+ */
+void gen_var_operator(token_t operator) {
+	if (operator== NULL) {
+		INFO("Operator is NULL!\n");
+		return;
+	}
+
+	switch (operator->type) {
+		case TK_PLUS:
+			INSTRUCTION("ADDS");
 			break;
-		case TK_FLOAT_LIT:
-			INSTRUCTION_PART("MOVE TF@", to, " float@");
-			printf("%a", param.f);
-			INSTRUCTION_END();
+		case TK_MINUS:
+			INSTRUCTION("SUBS");
 			break;
-		case TK_STR_LIT:
-			INSTRUCTION_PART("MOVE TF@", to, " string@");
-			encode_string_literal(param.s);
-			INSTRUCTION_END();
+		case TK_MULTIPLY:
+			INSTRUCTION("MULS");
 			break;
-		case TK_KEYW_TRUE:
-			INSTRUCTION("MOVE TF@", to, " bool@true");
-			break;
-		case TK_KEYW_FALSE:
-			INSTRUCTION("MOVE TF@", to, " bool@false");
+		case TK_DIVIDE:
+			INSTRUCTION("DIVS");
+			// TODO: For floats
+			// INSTRUCTION("IDIVS");
 			break;
 		default:
 			break;
 	}
+}
+
+/**
+ * Negates the last stack value
+ * @param type type of variable to be negated, one of {int, float}
+ */
+void gen_var_neg(token_type_t type) {
+	INSTRUCTION("POPS GF@rega");
+	INSTRUCTION("PUSHS ", tk_type2type[type], "@0");
+	INSTRUCTION("PUSHS GF@rega");
+	INSTRUCTION("SUBS");
+}
+
+void gen_var_not() {
+	INSTRUCTION("NOTS");
 }
--- src/generator.h
+++ src/generator.h
@@ -9,18 +9,20 @@ void gen_finish();
 
 /// Function definition
 void gen_func_begin(const char *identifier);
-void gen_func_init_frame();
 void gen_func_init_stack();
 void gen_func_param(const char *identifier);
 void gen_func_end();
 
 /// Function call
 void gen_func_call(const char *identifier);
-void gen_func_call_arg_literal(token_t token);
+void gen_func_call_arg(token_t token);
 
 /// Variable
 void gen_var_define(const char *identifier);
-void gen_var_assign_id(const char *to, const char *from);
-void gen_var_assign_literal(const char *to, tk_param_t param, token_type_t token_type);
+void gen_var_literal(token_t token);
+void gen_var_assign_expr_result(const char *identifier);
+void gen_var_operator(token_t operator);
+void gen_var_neg(token_type_t type);
+void gen_var_not();
 
 #endif //IFJ_GENERATOR_H
