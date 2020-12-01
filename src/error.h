84429a187ba9e3a64dd5ece551566967c9973972
KLZ-0 <adrian@kalazi.com>
2020-12-01 19:02:09 +0100

    Add debug macros

--- src/error.h
+++ src/error.h
@@ -7,14 +7,24 @@
 enum CompilerErrors
 {
 	ERROR_LEX = 1, ///< lexical errors
-	ERROR_SYN = 2, ///< syntactic errors
-	ERROR_DEFINITION = 3, ///< redefinition of identifier or undefined identifiers
-	ERROR_TYPE_INIT = 4, ///< new variable type mismatch
+	ERROR_SYN = 2,         ///< syntactic errors
+	ERROR_DEFINITION = 3,  ///< redefinition of identifier or undefined identifiers
+	ERROR_TYPE_INIT = 4,   ///< new variable type mismatch
 	ERROR_TYPE_COMPAT = 5, ///< arithmetic operation with uncompatible types
-	ERROR_PARAM = 6, ///< function return type, argument type or argument count mismatch
-	ERROR_SEM = 7, ///< other semantic errors
-	ERROR_ZERO_DIV = 9, ///< division by zero
-	ERROR_MISC = 99, ///< internal errors such as allocation error
+	ERROR_PARAM = 6,       ///< function return type, argument type or argument count mismatch
+	ERROR_SEM = 7,         ///< other semantic errors
+	ERROR_ZERO_DIV = 9,    ///< division by zero
+	ERROR_MISC = 99,       ///< internal errors such as allocation error
 };
+
+////// Macros
+
+#ifndef NDEBUG
+#define INFO(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define INFO(...) \
+	do {          \
+	} while (0)
+#endif
 
 #endif // !IFJ_ERROR_H
