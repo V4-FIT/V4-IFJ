11a7ca82bbd67d9910e926fdadf24c25f9d83959
Kevin Lackó <kevinlacko.kl@gmail.com>
2020-12-03 01:53:14 +0100

    add error messages for current semantic actions

--- src/semantics.c
+++ src/semantics.c
@@ -5,11 +5,13 @@
 int sem_define_func(parser_t parser) {
 	if (parser->first_pass) {
 		if (symtable_has_func(parser->symtable, parser->token)) {
+			PARSER_ERROR_MSG("The function '%s' has already been defined.", parser->token->param.s);
 			return ERROR_DEFINITION; /*redefinition*/
 		} else {
 			symbol_ref_t symbol = symtable_insert(parser->symtable, parser->token, ST_FUNC);
 			if (!symbol_valid(symbol)) {
-				return ERROR_MISC; /*allocation error*/
+				ALLOCATION_ERROR_MSG();
+				return ERROR_MISC;
 			}
 			parser->sem.func = symbol;
 		}
@@ -21,6 +23,7 @@ int sem_func_declare_param(parser_t parser) {
 	if (!parser->first_pass) {
 		symbol_ref_t symbol = symtable_insert(parser->symtable, parser->token, ST_VAR);
 		if (!symbol_valid(symbol)) {
+			ALLOCATION_ERROR_MSG();
 			return ERROR_MISC;
 		}
 		sym_var_t var_data = {0};
@@ -61,7 +64,12 @@ int sem_func_add_param_type(parser_t parser) {
 				break;
 		}
 	}
-	return success ? EXIT_SUCCESS : ERROR_MISC;
+	if (success) {
+		return EXIT_SUCCESS;
+	} else {
+		ALLOCATION_ERROR_MSG();
+		return ERROR_MISC;
+	}
 }
 
 int sem_func_add_return_type(parser_t parser) {
@@ -82,7 +90,12 @@ int sem_func_add_return_type(parser_t parser) {
 				break;
 		}
 	}
-	return success ? EXIT_SUCCESS : ERROR_MISC;
+	if (success) {
+		return EXIT_SUCCESS;
+	} else {
+		ALLOCATION_ERROR_MSG();
+		return ERROR_MISC;
+	}
 }
 
 int sem_func_callable(parser_t parser) {
@@ -91,9 +104,11 @@ int sem_func_callable(parser_t parser) {
 		if (symbol.symbol->type == ST_FUNC) {
 			parser->sem.func = symbol;
 		} else {
+			PARSER_ERROR_MSG("The function is hidden by the variable '%s'", parser->token->param.s);
 			return ERROR_SEM;
 		}
 	} else {
+		PARSER_ERROR_MSG("The function '%s' has not been defined.", parser->token->param.s);
 		return ERROR_DEFINITION;
 	}
 	return EXIT_SUCCESS;
@@ -105,9 +120,11 @@ int sem_main_defined(parser_t parser) {
 		symbol_ref_t symbol = symtable_find(parser->symtable, &tk);
 		if (symbol_valid(symbol)) {
 			if (symbol.symbol->func.param_count || symbol.symbol->func.return_count) {
+				PARSER_ERROR_MSG("The function 'main' should not have parameters or returns");
 				return ERROR_PARAM;
 			}
 		} else {
+			ERROR_MSG("The function 'main' has not been defined");
 			return ERROR_DEFINITION;
 		}
 	}
